beaa064e8a4da2226b87e5fac6f6fdd418d8d568
Aoife <aoife.bergin@traveltechlabs.com>
2020-04-07 23:45:57 +0100

    add lender and apr cards

--- dialogs/loanDialog.js
+++ dialogs/loanDialog.js
@@ -6,6 +6,7 @@ const { AmountPrompt } = require('../prompts/amountPrompt');
 // Import AdaptiveCard content.
 const BoiCard = require('./resources/boi.json');
 const BamlCard = require('./resources/baml.json');
+const CreditCard = require('./resources/creditUnion.json');
 
 const { TermPrompt } = require('../prompts/termPrompt');
 
@@ -38,7 +39,8 @@ const mseg = {
   // Create array of AdaptiveCard content, this will be used to send a random card to the user.
 const CARDS = [
     BoiCard,
-    BamlCard
+    BamlCard,
+    CreditCard
 ];
 class LoanDialog extends CancelAndHelpDialog {
     
@@ -56,11 +58,10 @@ class LoanDialog extends CancelAndHelpDialog {
             .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
                 this.nameStep.bind(this),
                 this.emailStep.bind(this),
-                this.cardStep.bind(this),
                 this.amountStep.bind(this),
                 this.termStep.bind(this),
-                this.lenderTypeStep.bind(this),
                 this.birthDateStep.bind(this),
+                this.lenderStep.bind(this),
                 this.confirmStep.bind(this),
                 this.finalStep.bind(this)
                 
@@ -123,18 +124,7 @@ class LoanDialog extends CancelAndHelpDialog {
     }
 
     
-    async lenderTypeStep(stepContext) {
-        const loanDetails = stepContext.options;
-
-        // Capture the response to the previous step's prompt
-        loanDetails.term = stepContext.result;
-        if (!loanDetails.lenderType) {
-            const messageText = 'From what type of lender would you like a loan?';
-            const msg = MessageFactory.text(messageText, 'From what type of lender would you like a loan?', InputHints.ExpectingInput);
-            return await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
-        }
-        return await stepContext.next(loanDetails.lenderType);
-    }
+  
 
     /**
      * If a travel date has not been provided, prompt for one.
@@ -150,6 +140,40 @@ class LoanDialog extends CancelAndHelpDialog {
         }
         return await stepContext.next(loanDetails.birthDate);
     }
+    async lenderStep( stepContext){
+
+        const loanDetails = stepContext.options;
+        loanDetails.birthDate = stepContext.result;
+        if (!loanDetails.APR) {
+        //this.onMessage(async (context, next) => {
+            let card;
+            if(`${ loanDetails.amount}`  <=3000)
+            {
+                card = CreditCard;
+                loanDetails.APR = 9.6;
+
+            }
+            else if (`${ loanDetails.amount}` <=5000)
+            {
+                card = BoiCard;
+                loanDetails.APR =  8.5;
+            }
+            else
+            {
+                card = BamlCard;
+                loanDetails.APR = 8.95;
+            }
+           // const randomlySelectedCard = CARDS[Math.floor((Math.random() * CARDS.length - 1) + 1)];
+            await stepContext.context.sendActivity({
+                text: 'Based on the informstion provided the lender that would best suit is:',
+                attachments: [CardFactory.adaptiveCard(card)]
+            });    
+        
+            // By calling next() you ensure that the next BotHandler is run.
+            return await stepContext.next(loanDetails.APR);
+          
+        }
+    }
 
     /**
      * Confirm the information the user has provided.
@@ -158,7 +182,7 @@ class LoanDialog extends CancelAndHelpDialog {
         const loanDetails = stepContext.options;
         if (!loanDetails.choice) {
         // Capture the results of the previous step
-        loanDetails.birthDate = stepContext.result;
+        loanDetails.APR = stepContext.result;
         const messageText = `Please confirm, I have you a loan for ${ loanDetails.amount} from: ${ loanDetails.lenderType } your birth date is: ${ loanDetails.birthDate }. Is this correct?`;
         const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
         // Offer a YES/NO prompt.
@@ -167,18 +191,7 @@ class LoanDialog extends CancelAndHelpDialog {
         return await stepContext.next(loanDetails.choice);
     }
    
-    async cardStep( stepContext){
-        //this.onMessage(async (context, next) => {
-            const randomlySelectedCard = CARDS[Math.floor((Math.random() * CARDS.length - 1) + 1)];
-            await stepContext.context.sendActivity({
-                text: 'Here is an Adaptive Card:',
-                attachments: [CardFactory.adaptiveCard(randomlySelectedCard)]
-            });  
-        
-            // By calling next() you ensure that the next BotHandler is run.
-            return await stepContext.next();
-        
-        }
+    
     /**
      * Complete the interaction and end the dialog.
      */
@@ -186,13 +199,13 @@ class LoanDialog extends CancelAndHelpDialog {
         
         if (stepContext.result === true) {
             const loanDetails = stepContext.options;
-            
+            console.log(`${loanDetails.APR}`);
             console.log(calcLoanAmount(`${loanDetails.term}`,`${loanDetails.amount}`));
 
 
 
 
-            const messageText = `Your Monthly payment would be: ${calcLoanAmount(`${loanDetails.term}`,`${loanDetails.amount}`)} , an email with quote details has been sent, do you wish to check you eligibility for a loan? `;
+            const messageText = `Your Monthly payment would be: ${calcLoanAmount(`${loanDetails.term}`,`${loanDetails.amount}`, `${loanDetails.APR}`)} , an email with quote details has been sent, do you wish to check you eligibility for a loan? `;
             const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
             // Offer a YES/NO prompt.
             return await stepContext.prompt(CONFIRM_PROMPT, { prompt: msg });
@@ -217,12 +230,12 @@ class LoanDialog extends CancelAndHelpDialog {
 
 
 
-function calcLoanAmount(loanTerm,loanAmount)
+function calcLoanAmount(loanTerm,loanAmount,rate)
 {
    // https://www.ifsautoloans.com/blog/car-loan-interest/
     const divisor = 12.00;
-    const APR = .10;
-    const interestRate = APR/divisor;
+  
+    const interestRate = rate/divisor;
     let middle = 1 + (interestRate);
     let term =loanTerm*divisor;
     let top = interestRate* loanAmount;
